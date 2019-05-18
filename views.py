2f4e720b97497977d3c38f10af2ef7d3b57b95f8
JianfuYang <Jianfu_Yang@163.com>
2019-05-18 17:37:49 +0800

    创建视图

--- views.py
+++ views.py
@@ -1,4 +1,9 @@
 from django.http import HttpResponse
+from django.shortcuts import redirect
+
 
 def index(request):
     return HttpResponse('index.html')
+
+def login(request):
+    return redirect('/index')
